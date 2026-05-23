0aec11e62f0ecf5ff849751263b9356ae8baeda1
Tuan Le <lctuan.0807@gmail.com>
2026-05-23 11:45:13 +0700

    add .env

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
+require "dotenv/load"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,7 +23,7 @@ Bundler.require(*Rails.groups)
 module ShopApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
