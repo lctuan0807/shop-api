ee73924f150b77d8675eb6b0ace5a057c3643c53
Tuan Le <lctuan.0807@gmail.com>
2026-06-02 13:17:45 +0700

    add discord logger

---
+++ lib/discord_bot.rb
@@ -0,0 +1,15 @@
+require 'discordrb'
+
+class DiscordBot
+  def self.start
+    bot = Discordrb::Bot.new(
+      token: ENV['DISCORD_BOT_TOKEN']
+    )
+
+    bot.message(content: '!ping') do |event|
+      event.respond 'Pong!'
+    end
+
+    bot.run
+  end
+end
