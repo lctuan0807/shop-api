7a352e3b9661d53774bf472eee7c9a431c3007cf
Tuan Le <lctuan.0807@gmail.com>
2026-06-10 12:08:59 +0700

    add docker setup

--- config/initializers/rabbitmq.rb
+++ config/initializers/rabbitmq.rb
@@ -1,5 +1,9 @@
-$rabbitmq_connection = Bunny.new(ENV["RABBITMQ_URL"])
-$rabbitmq_connection.start
+if Defined?(Bunny)
+  # Resolves automatically to amqp://your_user:pass@rabbitmq:5672 in production
+  rabbitmq_url = ENV.fetch("RABBITMQ_URL") { "amqp://guest:guest@localhost:5672" }
+  $rabbitmq_connection = Bunny.new(rabbitmq_url)
+  $rabbitmq_connection.start
 
-# Setup a channel for publishing
-$rabbitmq_channel = $rabbitmq_connection.create_channel
+  # Setup a channel for publishing
+  $rabbitmq_channel = $rabbitmq_connection.create_channel
+end
