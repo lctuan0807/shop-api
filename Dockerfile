ce2eb6933770c10181be841dbe0f1969fd099b27
Tuan Le <lctuan.0807@gmail.com>
2026-06-10 15:50:27 +0700

    add redis

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # syntax=docker/dockerfile:1
-ARG RUBY_VERSION=3.3.0
+ARG RUBY_VERSION=3.3.8
 FROM ruby:$RUBY_VERSION-slim AS base
 
 WORKDIR /rails
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"] }
+  config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
